95ae148fef832974d6e9e8596c7ba5117e602d12
rene-d <rene-d@users.noreply.github.com>
2022-09-05 06:12:35 +0200

    wip

--- ant/src/main.rs
+++ ant/src/main.rs
@@ -1,8 +1,7 @@
-//!
-//!  fourmi de Langton
+//! Langton's ant
+
 use std::collections::HashSet;
 use std::{thread, time};
-
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Position {
@@ -23,7 +22,6 @@ impl Game {
         if self.black.contains(&self.position) {
             self.direction = turn_right(self.direction);
             self.black.remove(&self.position);
-
         } else {
             self.direction = turn_left(self.direction);
             self.black.insert(self.position);
@@ -46,11 +44,10 @@ impl Game {
     }
 
     fn show(&self) {
-        print!("\x1B[H\x1B[2J");
+        print!("\x1B[H\x1B[2J\x1B[3J");
         println!(
-            "current: {:?}  direction: {}   box: {:?} {:?}",
-            self.position, self.direction,
-            self.pos_ll, self.pos_ur
+            "current: {:?}   direction: {}   box: {:?} {:?}",
+            self.position, self.direction, self.pos_ll, self.pos_ur
         );
         for y in (self.pos_ll.y..=self.pos_ur.y).rev() {
             for x in (self.pos_ll.x..=self.pos_ur.x).rev() {
@@ -108,12 +105,12 @@ fn main() {
 
     for i in 0..11500 {
         game.step();
-        if i % 1000 == 0         {
+
+        if i % 1000 == 0 {
             game.show();
 
-             let ten_millis = time::Duration::from_millis(100);
-             thread::sleep(ten_millis);
+            let ten_millis = time::Duration::from_millis(100);
+            thread::sleep(ten_millis);
         }
     }
-
- }
+}
